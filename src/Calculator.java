fb4dbb3a99523c68e1abd264874f53363019d852
Barak Ohayon <barakoh27@gmail.com>
2025-12-05 11:46:59 +0200

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -41,6 +41,8 @@ public class Calculator {
         public boolean isPrime(int num)
         {
             int x = 2;
+            if(num == 1 || num == 0)
+                return false;
             for(int i = 2; i < num; i++)
             {
                 if(num % i == 0)
